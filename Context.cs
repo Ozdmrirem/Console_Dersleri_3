ac33360f4d7ce03f0cb70190383fce7197b66748
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-03-16 22:57:25 +0300

    Add files via upload

---
+++ Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Concrete
+{
+    internal class Context: DbContext
+    {
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
