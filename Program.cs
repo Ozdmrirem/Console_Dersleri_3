7d275c5e970601d94a458c94919a424f44a33670
irem <146566414+Ozdmrirem@users.noreply.github.com>
2025-03-21 02:42:07 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -15,19 +15,19 @@ namespace Console_Dersleri_3
         static void Main(string[] args)
         {
             //Lokasyon Listeleme
-           LocationManager locationManager= new LocationManager (new EfLocationDal());
-            var values = locationManager.TGetList();
+            //LocationManager locationManager= new LocationManager (new EfLocationDal());
+            //var values = locationManager.TGetList();
 
-            void listele()
-            {
-                foreach (var value in values)
-                {
-                    Console.WriteLine(value.LocationName);
-                    Console.WriteLine("-------------------");
-                }
-            }
+            //void listele()
+            //{
+            //    foreach (var value in values)
+            //    {
+            //        Console.WriteLine(value.LocationName);
+            //        Console.WriteLine("-------------------");
+            //    }
+            //}
 
-            Location location = new Location();
+            //Location location = new Location();
             //location.LocationName = "Rotterdam";
             //locationManager.TInsert(location);
             //listele();
@@ -35,10 +35,42 @@ namespace Console_Dersleri_3
             //var locationValue = locationManager.TGetById(4);
             //locationManager.TDelete(locationValue);
 
-            var locationValue = locationManager.TGetById(2);
-            locationValue.LocationName = "Çanakkale";
-            locationManager.TUpdate(locationValue);
-            Console.Write("Güncellendi");
+            //var locationValue = locationManager.TGetById(2);
+            //locationValue.LocationName = "Çanakkale";
+            //locationManager.TUpdate(locationValue);
+            //Console.Write("Güncellendi");
+
+            //MemberManager memberManager = new MemberManager(new EfMemberDal());
+            //var values = memberManager.TGetList();
+            //foreach (var value in values) 
+            //    {
+            //        Console.WriteLine(value.MemberName + " " + value.MemberSurname);
+            //        Console.WriteLine("--------------------------");
+            //    }
+            //Member member=new Member();
+            //member.MemberName = "a";
+            //member.MemberSurname = "b";
+            //memberManager.TInsert(member);
+            //Console.WriteLine("Ekleme yapıldı");
+            //var valueMember = memberManager.TGetById(8);
+            //memberManager.TDelete(valueMember);
+            //foreach (var item in valueMember)
+            //{
+            //    Console.WriteLine(item.);
+            //}
+            //Console.WriteLine("işlem yapıldı");
+
+            //valueMember.MemberName = "Aslı";
+            //valueMember.MemberSurname = "Meşe";
+            //memberManager.TUpdate(valueMember);
+            //Console.WriteLine("İşlem yapıldı");
+
+            CommentManager commentManager = new CommentManager(new EfCommentDal());
+            var values = commentManager.TGetList();
+            foreach (var value in values)
+            {
+
+            }
 
             Console.ReadLine();
         }
